78f32f4a15e3ebfef677c8d03ac7c7b1f02ba7f5
CalebCoughenour <calebcoughenour@protonmail.com>
2025-05-11 23:35:29 -0700

    add stock and crypto dashboard routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from datetime import datetime
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", current_date=datetime.now().date())
+
+@main.route("/crypto")
+def crypto_dashboard():
+    return render_template("crypto_dashboard.html", current_date=datetime.now().date())
+
+@main.route("/stocks")
+def stock_dashboard():
+    return render_template("stock_dashboard.html", current_date=datetime.now().date())
