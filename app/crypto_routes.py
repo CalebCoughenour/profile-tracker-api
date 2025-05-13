2f3debe19bc56e274f810aba3a9060473fdb1cc5
CalebCoughenour <calebcoughenour@protonmail.com>
2025-05-12 17:15:31 -0700

    add route for crypto price checker for btc, eth & avax

--- app/crypto_routes.py
+++ app/crypto_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+import requests
 
 crypto = Blueprint("crypto", __name__, url_prefix="/crypto")
 
@@ -9,3 +10,19 @@ def crypto_home():
 @crypto.route("/portfolio-dashboard")
 def crypto_dashboard():
     return render_template("crypto/crypto_dashboard.html")
+
+@crypto.route("/ticker-prices")
+def ticker_prices():
+    coin_ids = "bitcoin,ethereum,avalanche-2"
+    vs_currency = "usd"
+    api_url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_ids}&vs_currencies={vs_currency}"
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        prices = response.json()
+    except Exception as e:
+        print(f"Error {e}")
+        prices = {}
+
+    return jsonify(prices)
